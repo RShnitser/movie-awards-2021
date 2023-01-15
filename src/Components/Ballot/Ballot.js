4096e4e187ac2722d79da49931d06ad3222220a8
Roman Shnitser <romanshnitser@hotmail.com>
2023-01-14 23:22:13 -0500

    add get ballots api call

--- src/Components/Ballot/Ballot.js
+++ src/Components/Ballot/Ballot.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from "../../Api/Api"
+//import { useEffect, useState } from 'react/cjs/react.production.min';
 
 const Ballot = () => {
+
+  const [ballots, setBallots] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // const fetchBallots = async () => {
+    //   try {
+    //     setLoading(true);
+    //     const response = await fetch('http://localhost:3000/api/getBallotData');
+    //     if(response.ok) {
+    //       const data = await response.json();
+    //       setBallots(data);
+    //       setLoading(false);
+    //     }
+
+    //   }
+    //   catch(error) {
+    //     console.error(error);
+    //   }
+    // }
+
+    // fetchBallots();
+    setLoading(true);
+    api.getBallotData()
+      .then((data) => {
+        setBallots(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+
+  }, []);
+
   return (
     <div className='ballot'>
-      'Your Code Goes Here'
+      {!loading ? ballots.items.map((category) => {
+
+          return(
+            <div>{category.title}</div>
+          );
+        }): null}
     </div>
   )
 }
